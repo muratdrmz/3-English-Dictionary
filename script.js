4427c380845ea10e6f0275c67d4a7ab01956d4eb
muratdrmz <mrtdrmz@windowslive.com>
2023-01-22 10:36:02 +0100

    22/01-10:35

---
+++ script.js
@@ -0,0 +1,18 @@
+const inputEl=document.getElementById('input');
+
+async function fetchAPI(word){
+ try {
+  const url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  const result= await fetch(url).then((res)=>res.json())
+  console.log(result);  
+ } catch (error) {
+  console.log(error);
+ }
+}
+
+inputEl.addEventListener('keyup', (e)=>{
+if(e.target.value && e.key==='Enter'){
+ fetchAPI(e.target.value)
+}
+
+})
